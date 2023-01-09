5c90f455bfbc3ec6d2402d9059ca633fb0f8fab2
lestrangeqq <lestrangeqq@gmail.com>
2023-01-09 20:12:25 +0200

    Add function to mark done set. Refactoring

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const sets = document.querySelectorAll('#sets span');
+
 function calcSet() {
 	let firstTry = document.getElementById('first-try').value;
 
@@ -5,13 +7,30 @@ function calcSet() {
 		messanger.innerHTML = 'Invalid value';
 	}
 	
-	document.getElementById('set1').innerHTML = Math.ceil(firstTry - firstTry / 100 * 30);
-	document.getElementById('set2').innerHTML = Math.ceil(firstTry - firstTry / 100 * 15);
-	document.getElementById('set3').innerHTML = Math.ceil(firstTry - firstTry / 100 * -10);
-	document.getElementById('set4').innerHTML = Math.ceil(firstTry - firstTry / 100 * 35);
-	document.getElementById('set5').innerHTML = Math.ceil(firstTry - firstTry / 100 * 50);
+	sets[0].innerHTML = Math.ceil(firstTry - firstTry / 100 * 30);
+	sets[1].innerHTML = Math.ceil(firstTry - firstTry / 100 * 15);
+	sets[2].innerHTML = Math.ceil(firstTry - firstTry / 100 * -5);
+    sets[3].innerHTML = Math.ceil(firstTry - firstTry / 100 * 35);
+	sets[4].innerHTML = Math.ceil(firstTry - firstTry / 100 * 50);
 
+    hideInfo()
+}
+
+function hideInfo() {
+    document.getElementById('form').style.display = 'none';
 	document.getElementById('messanger').innerHTML = "After every set make 1 minute rest (or 30 sec. if it's very easy for you).";
+
+    markDone()
+}
+
+function setDone() {
+    this.style.background = 'lime';
+}
+
+function markDone() {
+    for (i=0; i<5; i++) {
+        sets[i].addEventListener('click', setDone);
+    }
 }
 
 document.getElementById('submit-try').addEventListener('click', calcSet);
